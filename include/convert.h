290e40387a7e4f3e8d28d3a7910868a912623c41
Michael H. Potter <michael@potterpcs.net>
2019-03-11 02:00:31 -0400

    A simple function to find the lowest digit

---
+++ include/convert.h
@@ -0,0 +1,25 @@
+#ifndef TRIREME_CONVERT_H
+#define TRIREME_CONVERT_H
+
+#include <cstdlib>
+
+/**
+ * @brief Returns the lowest trit for an integer.
+ * 
+ * @param value Any signed integer
+ * @return constexpr int A value {1, 0, -1} representing the lowest balanced trit.
+ */
+constexpr int lowest_trit(int value)
+{
+    auto temp = value % 3;
+
+    if (std::abs(temp) != 2) {
+        return temp;
+    }
+    else
+    {
+        return (value > 0) ? -1 : 1;
+    }
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+#include "convert.h"
+
 int main(int, char**) {
-    auto h = "Hello";
-    std::cout << h << ", world!\n";
+    auto testvalues = {0, 1, 2, 4, 8, -5, -12, -13};
+    for (auto&& v : testvalues)
+    {
+        std::cout << lowest_trit(v) << '\n';
+    }
 }
