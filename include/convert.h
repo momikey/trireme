6a7e3223a77eaa37a364a8b4785d88c1e3bbc89e
Michael H. Potter <michael@potterpcs.net>
2019-03-11 19:49:22 -0400

    Constexpr conversion of an array of trits to an integer value

--- include/convert.h
+++ include/convert.h
@@ -3,18 +3,38 @@
 
 #include <cstdlib>
 #include <array>
+#include <utility>
+
+/**
+ * @brief Constexpr absolute value function.
+ * 
+ * @param value Any integer
+ * @return constexpr int The absolute value of the given integer
+ */
+constexpr int abs_c(int value)
+{
+    if (value >= 0)
+    {
+        return value;
+    }
+    else
+    {
+        return -value;
+    }
+    
+}
 
 /**
  * @brief Returns the lowest trit for an integer.
  * 
  * @param value Any signed integer
- * @return constexpr int A value {1, 0, -1} representing the lowest balanced trit.
+ * @return constexpr int A value {1, 0, -1} representing the lowest balanced trit
  */
 constexpr int lowest_trit(int value)
 {
     auto temp = value % 3;
 
-    if (std::abs(temp) != 2) {
+    if (abs_c(temp) != 2) {
         return temp;
     }
     else
@@ -26,6 +46,20 @@ constexpr int lowest_trit(int value)
 constexpr int pow3(unsigned int exponent)
 {
     return (exponent == 0) ? 1 : 3 * pow3(exponent - 1);
+}
+
+template<typename Int, std::size_t Size>
+constexpr Int to_binary_impl(const std::array<Int, Size>& arr, std::size_t level)
+{
+    return (level == 0)
+        ? arr[level]
+        : arr[level] * pow3(level) + to_binary_impl(arr, level-1);
+}
+
+template<typename Int, std::size_t Size>
+constexpr Int to_binary(const std::array<Int, Size>& arr)
+{
+    return to_binary_impl(arr, Size-1);
 }
 
 #endif /* TRIREME_CONVERT_H */
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <array>
 
 #include "convert.h"
+
+template<typename T, std::size_t N>
+void print_array(const std::array<T,N>& arr)
+{
+    for (auto&& v : arr)
+    {
+        std::cout << v << ',';
+    }
+    std::cout << '\n';
+}
 
 int main(int, char**) {
     auto testvalues = {0, 1, 2, 4, 8, -5, -12, -13};
@@ -14,4 +25,7 @@ int main(int, char**) {
     {
         std::cout << pow3(v) << '\n';
     }
+
+    constexpr std::array<int, 9> arr = {1, 1, 0, -1, 1, 0, 0, 1, -1};
+    std::cout << to_binary(arr) << '\n';
 }
