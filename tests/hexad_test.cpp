69199b9d00ab66cdebf68f4de312fedf1f72e28f
Michael H. Potter <michael@potterpcs.net>
2019-03-30 19:52:52 -0400

    Finished Hexad test cases

--- tests/hexad_test.cpp
+++ tests/hexad_test.cpp
@@ -1,4 +1,6 @@
 #include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/data/monomorphic.hpp>
 
 #include <array>
 #include <utility>
@@ -6,6 +8,7 @@
 #include "hexad.hpp"
 
 using ternary::Hexad;
+namespace bdata = boost::unit_test::data;
 
 struct HexadFixture
 {
@@ -17,7 +20,13 @@ struct HexadFixture
 
     Hexad largePositive { 255 };
     Hexad largeNegative { -321 };
+
+    // Identifiable pattern: +0-+0-
+    Hexad tritPattern { 224 };
 };
+
+// Note that these are low to high
+constexpr std::array<int, 6> tritsInPattern { -1, 0, 1, -1, 0, 1 };
 
 BOOST_FIXTURE_TEST_SUITE(hexad, HexadFixture)
 
@@ -140,9 +149,248 @@ BOOST_FIXTURE_TEST_SUITE(hexad, HexadFixture)
         auto r { Hexad::range };
         BOOST_TEST(left_shift(smallPositive).get() == smallPositive.get() * 3);
         BOOST_TEST(left_shift(largePositive).get() == largePositive.get() * 3 - r);
-        BOOST_TEST(left_shift(largeNegative).get() == largeNegative.get() * 3 -+ r);
+        BOOST_TEST(left_shift(largeNegative).get() == largeNegative.get() * 3 + r);
     }
-      
+
+    BOOST_AUTO_TEST_CASE(operation_right_shift_single)
+    {
+        BOOST_TEST(right_shift(smallPositive).get() == smallPositive.get() / 3);
+        BOOST_TEST(right_shift(largePositive).get() == largePositive.get() / 3);
+        BOOST_TEST(right_shift(largeNegative).get() == largeNegative.get() / 3);
+    }
+    
+
+    BOOST_AUTO_TEST_CASE(operation_left_shift_multiple)
+    {
+        for (auto i = 1u; i <= Hexad::width; ++i)
+        {
+            BOOST_TEST(left_shift(tritPattern, i).second == tritsInPattern[Hexad::width - i]);
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(operation_right_shift_multiple)
+    {
+        for (auto i = 1u; i <= Hexad::width; ++i)
+        {
+            BOOST_TEST(right_shift(tritPattern, i).second == tritsInPattern[i - 1]);
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(operation_left_rotate)
+    {
+        for (auto i = 1u; i <= Hexad::width; ++i)
+        {
+            BOOST_TEST(rotate_left(tritPattern, i).trits()[0] == tritsInPattern[Hexad::width - i]);
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(operation_right_rotate)
+    {
+        for (auto i = 1u; i <= Hexad::width; ++i)
+        {
+            BOOST_TEST(rotate_right(tritPattern, i).trits()[0] == tritsInPattern[i % Hexad::width]);
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(operation_left_rotate_carry)
+    {
+        for (auto i = 1u; i <= Hexad::width; ++i)
+        {
+            BOOST_TEST(rotate_left_carry(tritPattern, 0, i).second == tritsInPattern[Hexad::width - i]);
+            
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(operation_right_rotate_carry)
+    {
+        for (auto i = 1u; i <= Hexad::width; ++i)
+        {
+            BOOST_TEST(rotate_right_carry(tritPattern, 0, i).second == tritsInPattern[i - 1]);
+            
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(operation_simple_invert)
+    {
+        BOOST_TEST(invert(smallPositive).get() == -smallPositive.get());
+        BOOST_TEST(invert(largeNegative).get() == -largeNegative.get());
+    }
+    
+    BOOST_AUTO_TEST_CASE(operation_positive_invert)
+    {
+        auto inv1 { positive_invert(smallPositive) };
+        auto inv2 { positive_invert(largeNegative) };
+
+        for (auto i = 0u; i < Hexad::width; ++i)
+        {
+            if (smallPositive.trits()[i] == 1)
+            {
+                BOOST_TEST(inv1.trits()[i] == -1);
+            }
+            else
+            {
+                BOOST_TEST(inv1.trits()[i] == 1);
+            }
+
+            if (largeNegative.trits()[i] == 1)
+            {
+                BOOST_TEST(inv2.trits()[i] == -1);
+            }
+            else
+            {
+                BOOST_TEST(inv2.trits()[i] == 1);
+            }
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(operation_negative_invert)
+    {
+        auto inv1 { negative_invert(smallPositive) };
+        auto inv2 { negative_invert(largeNegative) };
+
+        for (auto i = 0u; i < Hexad::width; ++i)
+        {
+            if (smallPositive.trits()[i] == -1)
+            {
+                BOOST_TEST(inv1.trits()[i] == -1);
+            }
+            else
+            {
+                BOOST_TEST(inv1.trits()[i] == -1);
+            }
+
+            if (largeNegative.trits()[i] == -1)
+            {
+                BOOST_TEST(inv2.trits()[i] == 1);
+            }
+            else
+            {
+                BOOST_TEST(inv2.trits()[i] == -1);
+            }
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(operation_forward_diode)
+    {
+        auto d1 { forward_diode(smallPositive) };
+        auto d2 { forward_diode(largeNegative) };
+
+        for (auto i = 0u; i < Hexad::width; ++i)
+        {
+            if (smallPositive.trits()[i] == 1)
+            {
+                BOOST_TEST(d1.trits()[i] == 1);
+            }
+            else
+            {
+                BOOST_TEST(d1.trits()[i] == 0);
+            }
+
+            if (largeNegative.trits()[i] == 1)
+            {
+                BOOST_TEST(d2.trits()[i] == 1);
+            }
+            else
+            {
+                BOOST_TEST(d2.trits()[i] == 0);
+            }
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(operation_reverse_diode)
+    {
+        auto d1 { reverse_diode(smallPositive) };
+        auto d2 { reverse_diode(largeNegative) };
+
+        for (auto i = 0u; i < Hexad::width; ++i)
+        {
+            if (smallPositive.trits()[i] == -1)
+            {
+                BOOST_TEST(d1.trits()[i] == -1);
+            }
+            else
+            {
+                BOOST_TEST(d1.trits()[i] == 0);
+            }
+
+            if (largeNegative.trits()[i] == -1)
+            {
+                BOOST_TEST(d2.trits()[i] == -1);
+            }
+            else
+            {
+                BOOST_TEST(d2.trits()[i] == 0);
+            }
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(operation_trit_maximum)
+    {
+        auto result1 { trit_maximum(smallPositive, smallNegative) };
+        auto result2 { trit_maximum(largePositive, largeNegative) };
+
+        for (auto i = 0u; i < Hexad::width; ++i)
+        {
+            BOOST_TEST(result1.trits()[i] ==
+                std::max(smallPositive.trits()[i], smallNegative.trits()[i])
+            );
+
+            BOOST_TEST(result2.trits()[i] ==
+                std::max(largePositive.trits()[i], largeNegative.trits()[i])
+            );
+        }
+    }
+    
+    BOOST_AUTO_TEST_CASE(operation_trit_minimum)
+    {
+        auto result1 { trit_minimum(smallPositive, smallNegative) };
+        auto result2 { trit_minimum(largePositive, largeNegative) };
+
+        for (auto i = 0u; i < Hexad::width; ++i)
+        {
+            BOOST_TEST(result1.trits()[i] ==
+                std::min(smallPositive.trits()[i], smallNegative.trits()[i])
+            );
+
+            BOOST_TEST(result2.trits()[i] ==
+                std::min(largePositive.trits()[i], largeNegative.trits()[i])
+            );
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(operation_logical_equality)
+    {
+        auto result1 { logical_equality(smallPositive, smallNegative) };
+        auto result2 { logical_equality(largePositive, largeNegative) };
+
+        for (auto i = 0u; i < Hexad::width; ++i)
+        {
+            BOOST_TEST(result1.trits()[i] ==
+                (smallPositive.trits()[i] == smallNegative.trits()[i] ? 1 : -1)
+            );
+
+            BOOST_TEST(result2.trits()[i] ==
+                (largePositive.trits()[i] == largeNegative.trits()[i] ? 1 : -1)
+            );
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(operation_logical_multiply)
+    {
+        auto result1 { logical_multiply(smallPositive, smallNegative) };
+        auto result2 { logical_multiply(largePositive, largeNegative) };
+
+        for (auto i = 0u; i < Hexad::width; ++i)
+        {
+            BOOST_TEST(result1.trits()[i] ==
+                (smallPositive.trits()[i] * smallNegative.trits()[i])
+            );
+
+            BOOST_TEST(result2.trits()[i] ==
+                (largePositive.trits()[i] * largeNegative.trits()[i])
+            );
+        }
+    }
 
     BOOST_AUTO_TEST_SUITE_END()
 
